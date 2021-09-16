138ec07ff9dd170d9d6acefc648f386aed8522e2
Ilya Trifonov <41755260+ilya-trifonov@users.noreply.github.com>
2021-09-16 11:27:25 +0300

    some changes

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 
-// use process.env variables to keep private variables,
 require('dotenv').config()
 
-// Express Middleware
-const helmet = require('helmet') // creates headers that protect from attacks (security)
-const bodyParser = require('body-parser') // turns response into usable format
-const cors = require('cors')  // allows/disallows cross-site communication
-const morgan = require('morgan') // logs requests
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
 // db Connection w/ Heroku
 // const db = require('knex')({
@@ -29,13 +27,10 @@ var db = require('knex')({
   }
 });
 
-// Controllers - aka, the db queries
 const main = require('./controllers/main')
 
-// App
 const app = express()
 
-// App Middleware
 const whitelist = ['http://localhost:3000']
 const corsOptions = {
   origin: function (origin, callback) {
@@ -51,14 +46,12 @@ app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(morgan('combined')) // use 'tiny' or 'combined'
 
-// App Routes - Auth
 app.get('/', (req, res) => res.send('hello world'))
 app.get('/crud', (req, res) => main.getTableData(req, res, db))
 app.post('/crud', (req, res) => main.postTableData(req, res, db))
 app.put('/crud', (req, res) => main.putTableData(req, res, db))
 app.delete('/crud', (req, res) => main.deleteTableData(req, res, db))
 
-// App Server Connection
 app.listen(process.env.PORT || 8000, () => {
   console.log(`app is running on port ${process.env.PORT || 8000}`)
 })
